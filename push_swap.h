2b8c3a85cbbb3ecc7292f2ede3eb1575a030f306
Yassine fahfouhi <yassinefahfouhi@Yassines-MacBook-Pro.local>
2024-12-09 23:43:52 +0100

    push function fixed

--- push_swap.h
+++ push_swap.h
@@ -6,13 +6,15 @@
 /*   By: yassinefahfouhi <yassinefahfouhi@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 19:35:28 by yassinefahf       #+#    #+#             */
-/*   Updated: 2024/12/09 16:21:32 by yassinefahf      ###   ########.fr       */
+/*   Updated: 2024/12/09 20:24:32 by yassinefahf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 #include <stdlib.h>
+#include <stdio.h>
+
 typedef struct s_stack{
 
 	int	size;
@@ -23,7 +25,8 @@ typedef struct s_stack{
 } t_stack;
 
 void	swap(t_stack s);
-void	push_a(t_stack s1, t_stack s2);
+void	push(t_stack *s1, t_stack *s2);
+void	rotate(t_stack s);
 
 
 #endif
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: yassinefahfouhi <yassinefahfouhi@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 15:54:25 by yassinefahf       #+#    #+#             */
-/*   Updated: 2024/12/09 17:32:23 by yassinefahf      ###   ########.fr       */
+/*   Updated: 2024/12/09 23:43:20 by yassinefahf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,47 +25,58 @@ void	swap(t_stack s)
 		s.tab[size - 1] = tmp;
 	}
 }
-void	push_a(t_stack s1, t_stack s2)
+void	push(t_stack *s1, t_stack *s2)
 {
-	// t_stack	tmp;
-
-	if (s2.size > 0)
+	if (s2->size > 0)
 	{
-		s1.size++;
-		s1.tab[s1.size - 1] = s2.tab[s2.size - 1];
-		s2.size--;
-		//pas besoin de trier le top est le dernier element
+		s1->tab[s1->size] = s2->tab[s2->size - 1];
+		s1->size += 1;
+		s2->size--;
 	}
-// decaler tout a droite pour pouvoir mettre s2[0] au top dans la stack a
 }
 void	swap_both(t_stack s1, t_stack s2)
 {
 	swap(s1);
 	swap(s2);
 }
-#include <stdio.h>
+void	rotate(t_stack s)
+{
+	int	i;
+	int	tmp;
+	int	first;
+
+	i = 0;
+	first = s.tab[s.size - 1];
+	while (i < s.size)
+	{
+		
+	}
+}
 void	print_stack(t_stack s)
 {
+	printf("size: %d\n", s.size);
 	for (int i = 0; i < s.size; i++)
+	{
 		printf("%d\n", s.tab[i]);
+	}
 }
 int	main()
 {
 	t_stack	s;
 	t_stack	s2;
 
-	s.tab = (int *)malloc(15);
-	s2.tab = (int *)malloc(2);
+	s.tab = (int *)malloc(15 * sizeof(int));
+	s2.tab = (int *)malloc(2 * sizeof(int));
 	s.size = 9;
 	s2.size = 2;
 	s2.tab[0] = 9;
 	// printf("%d\n", s2.tab[0]);
-	// s2.tab[1] = 11;
+	s2.tab[1] = 11;
 	for (int i = 0; i < 9; i++)
 		s.tab[i] = i;
 	// swap(s);
-	print_stack(s2);
-	// push_a(s, s2);
 	// print_stack(s);
-
+	push(&s, &s2);
+	swap_both(s, s2);
+	print_stack(s);
 }
